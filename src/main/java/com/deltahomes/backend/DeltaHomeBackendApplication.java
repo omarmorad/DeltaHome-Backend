785a778ce69f34c45f8d59da40c8b8abca360dee
Omar Morad <omorad5362@gmail.com>
2026-07-30 21:02:25 +0300

    app entry

---
+++ src/main/java/com/deltahomes/backend/DeltaHomeBackendApplication.java
@@ -0,0 +1,16 @@
+package com.deltahomes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableCaching
+public class DeltaHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaHomeBackendApplication.class, args);
+    }
+}
