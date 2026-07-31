e17537a6e435e0af1e2382227809da4629da87f3
Omar Morad <omorad5362@gmail.com>
2026-07-31 11:18:29 +0300

    empty initial test

---
+++ src/test/java/com/deltahomes/backend/DeltaHomeBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.deltahomes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DeltaHomeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
